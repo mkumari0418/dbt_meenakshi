d6f68ad6fc7affe54e595b5d5dabbcef833d003c
meenakshi.kumari@tredence.com <meenakshi.kumari@tredence.com>
2024-10-23 18:07:03 +0000

    incremental strategy validations in BQ

--- models/example/dept_hist_dim.sql
+++ models/example/dept_hist_dim.sql
@@ -2,7 +2,8 @@
 
 {{ config(
     materialized='incremental' 
- ,incremental_strategy = 'append'
+ , incremental_strategy = "merge"
+ ,unique_key=['department_num']
 )
 }}
 
@@ -12,10 +13,12 @@ with source_data as (
 
 ),
 max_sk as (
-    Select max(department_num) as max_skey from studio-de-accelerator.dbt_mkumari.dept_hist_stg
+    Select max(department_num) as max_skey from studio-de-accelerator.dbt_mkumari.dept_hist_dim
 )
 
-select (
-select max_skey from max_sk) + Row_number() over(order by  1) as department_num,hist_department_short_desc, hist_department_long_desc, hist_cost_department_ind, hist_major_dept_grp_num, hist_major_dept_grp_short_desc, hist_major_dept_grp_long_desc, hist_mega_dept_grp_num, hist_mega_dept_grp_short_desc, hist_mega_dept_grp_long_desc, op_ts
+select 
+-- (select max_skey from max_sk) + Row_number() over(order by  1) as 
+department_num,hist_department_short_desc, hist_department_long_desc, hist_cost_department_ind, hist_major_dept_grp_num, hist_major_dept_grp_short_desc, hist_major_dept_grp_long_desc, hist_mega_dept_grp_num, hist_mega_dept_grp_short_desc, hist_mega_dept_grp_long_desc, op_ts,
+current_datetime() as updated_ts
 from source_data
 
