9830f9cb65b75d783c9305eaa47cbe094a573cf7
meenakshi.kumari@tredence.com <meenakshi.kumari@tredence.com>
2024-10-24 14:11:58 +0000

    commit

--- models/landing/parsed_json_test_lnd.sql
+++ models/landing/parsed_json_test_lnd.sql
@@ -5,10 +5,10 @@
 )
 }}
 
-SELECT    Replace( JSON_EXTRACT(parsed_records.after, '$.op_type'),'"','') as op_type , 
-cast(Replace( JSON_EXTRACT(parsed_records.after, '$.MEMBERSHIPNUMBER'),'"','') as INT64) as MEMBERSHIPNUMBER,
-cast(Replace( JSON_EXTRACT(parsed_records.after, '$.CHGSTAMP'),'"','') as Datetime) as CHGSTAMP,
-cast(Replace( JSON_EXTRACT(parsed_records.after, '$.REDEMPTIONAMOUNT'),'"','') as FLOAT64) as REDEMPTIONAMOUNT,
+SELECT     JSON_VALUE(parsed_records.after, '$.op_type') as op_type , 
+cast(JSON_VALUE(parsed_records.after, '$.MEMBERSHIPNUMBER') as INT64) as MEMBERSHIPNUMBER,
+cast(JSON_VALUE(parsed_records.after, '$.CHGSTAMP') as Datetime) as CHGSTAMP,
+cast( JSON_VALUE(parsed_records.after, '$.REDEMPTIONAMOUNT') as FLOAT64) as REDEMPTIONAMOUNT,
  JSON_VALUE(parsed_records.after, '$.DBACTION') as DBACTION,
 current_datetime () as updated_ts
 FROM `studio-de-accelerator.dbt_mkumari.raw_cdc_test_json` 
