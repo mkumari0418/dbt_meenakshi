633a9bb4085659abda3be5763ff699ee80db29cb
meenakshi.kumari@tredence.com <meenakshi.kumari@tredence.com>
2024-10-24 13:05:04 +0000

    json testing

--- models/landing/parsed_json_test.sql
+++ models/landing/parsed_json_test.sql
@@ -1,14 +1,14 @@
 {{ config(
     materialized='incremental' 
  , incremental_strategy = "insert_overwrite"
- 
+  ,partition_by={"field": "updated_ts", "data_type": "Datetime"} 
 )
 }}
 
-SELECT    Replace( JSON_EXTRACT(parsed_records.after, '$.table'),'"','') as op_type , 
+SELECT    Replace( JSON_EXTRACT(parsed_records.after, '$.op_type'),'"','') as op_type , 
 cast(Replace( JSON_EXTRACT(parsed_records.after, '$.MEMBERSHIPNUMBER'),'"','') as INT64) as MEMBERSHIPNUMBER,
 cast(Replace( JSON_EXTRACT(parsed_records.after, '$.CHGSTAMP'),'"','') as Datetime) as CHGSTAMP,
-cast(Replace( JSON_EXTRACT(parsed_records.after, '$.REDEMPTIONAMOUNT'),'"','') as INT64) as REDEMPTIONAMOUNT,
+cast(Replace( JSON_EXTRACT(parsed_records.after, '$.REDEMPTIONAMOUNT'),'"','') as FLOAT64) as REDEMPTIONAMOUNT,
 cast(Replace( JSON_EXTRACT(parsed_records.after, '$.DBACTION'),'"','') as STRING) as DBACTION,
 current_datetime () as updated_ts
 FROM `studio-de-accelerator.dbt_mkumari.raw_cdc_test_json` 
